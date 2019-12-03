4d90ed5b65331ce37f39c6afc5c1c4c9c5f5a6d4
xzwb <1478863479@qq.com>
2019-12-03 19:19:14 +0800

    helloworld

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String name = request.getParameter("name");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>helloworld</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.printf("<h1>hello！%s%n</h1>", name);
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
