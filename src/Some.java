5bf7c9a7cefe7bef7db739f8bcdded1afdd5fc39
xzwb <1478863479@qq.com>
2019-12-10 11:44:04 +0800

    一个Servlet包括另一个Servlet

---
+++ src/Some.java
@@ -0,0 +1,27 @@
+import javax.print.attribute.standard.RequestingUserName;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "Some",
+        urlPatterns = {"/some"}
+)
+public class Some extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Some do one....");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("other");
+        requestDispatcher.include(request, response);
+        out.println("Some do two...");
+    }
+}
