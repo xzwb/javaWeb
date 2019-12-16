cd96dd25d5c168e2c1a077efebf14874e8ee3184
xzwb <1478863479@qq.com>
2019-12-16 16:14:48 +0800

    getReader方法感觉对于get请求没啥用

---
+++ src/Test1.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        name = "Test1",
+        urlPatterns = {"/test1"},
+        loadOnStartup = 1
+)
+/**
+ * req.getReader()方法对于get请求没啥用感觉
+ */
+public class Test1 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.println(bodyContent(req.getReader()));
+    }
+
+    private String bodyContent(BufferedReader reader)
+        throws IOException {
+        String input = null;
+        StringBuilder requestBody = new StringBuilder();
+        while ((input = reader.readLine()) != null) {
+            requestBody.append(input).append("<br>");
+        }
+        System.out.println(requestBody);
+        return requestBody.toString();
+    }
+}
